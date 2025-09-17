e127c6992b252aead247752d07d60bdf04bbf61a
Kiran <kiran.sadara6730@gmail.com>
2025-09-17 18:22:40 +0530

    Course db details added

--- backend/database/users.js
+++ backend/database/users.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
+import Course from "./course.js";
 
 const user_schema = new mongoose.Schema({
 
     username:{type:String,required:true},
       
     email:{type:String , required:true},
-    password :{type:string ,required:true}
+    password :{type:string ,required:true},
+    courses :[{
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:'Course'   
+    }]
 
 })
 
